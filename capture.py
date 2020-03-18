201e175f46c75bb636e01c88283e6a8d0715748a
Al Francis Gabriel Bolima <alfrancisgabriel@gmail.com>
2020-03-18 14:17:36 +0800

    patched

--- capture.py
+++ capture.py
@@ -13,7 +13,7 @@ name=''
 def capture(name):
     face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
     cap = cv2.VideoCapture(0)
-    
+
 
     def capko():
         num =0
@@ -23,27 +23,47 @@ def capture(name):
             faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
             for (x,y,w,h) in faces:
-                print(x,y,w,h)
+                #print(x,y,w,h)
                 roi_gray = gray[y:y+h, x:x+w]
                 #roi_color = frame[y:y+h, x:x+w]
                 img_item = (f'.\images\{name}\{num}.png')
                 cv2.imwrite(img_item, roi_gray)
-                
+
                 color = (255, 0,0)
                 stroke = 2
                 end_cord_x = x+w
                 end_cord_y = y+h
                 cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y),color,stroke)
                 num = num+1
-                print(num)
+                #print(num)
 
             cv2.imshow('frame',frame)
-            if cv2.waitKey(20) & 0xFF == ord('q') or num>=300:
+            if num==500*.10:
+                print('10% training completed!')
+            elif num==500*.20:
+                print('20% training completed!')
+            elif num==500*.30:
+                print('30% training completed!')
+            elif num==500*.40:
+                print('40% training completed!')
+            elif num==500*.50:
+                print('50% training completed!')
+            elif num==500*.60:
+                print('60% training completed!')
+            elif num==500*.70:
+                print('70% training completed!')
+            elif num==500*.80:
+                print('80% training completed!')
+            elif num==500*.90:
+                print('90% training completed!')
+            elif num==500*.100:
+                print('Training completed!')
+            if cv2.waitKey(20) & 0xFF == ord('q') or num>=500:
                 break
-            
+
         cap.release()
         cv2.destroyAllWindows()
-        
+
     if not os.path.exists(f'images/{name}'):
         os.makedirs(f'images/{name}')
         capko()
@@ -55,5 +75,3 @@ def capture(name):
         msgBox.setStandardButtons(QMessageBox.Ok)
         msgBox.exec()
         os.system("python main.py")
-
-        
--- faces.py
+++ faces.py
@@ -34,20 +34,20 @@ def detect(target_name):
     cap = cv2.VideoCapture(0)
 
     while(True):
-        
+
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=5)
-        
+
         for (x,y,w,h) in faces:
             #print(x,y,w,h)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = frame[y:y+h, x:x+w]
             id_, conf = recognizer.predict(roi_gray)
-            print(conf)
+            #print(conf)
             if conf>=80 or conf<=95:# and conf<100:
-                print(id_)
-                print(labels[id_])
+                #print(id_)
+                print(f'detected:{labels[id_]}')
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 name = labels[id_]
                 color = (255 ,255, 255)
@@ -61,6 +61,9 @@ def detect(target_name):
 
                 if isAthenticated>=10:
                     print("Verification success!")
+                elif isAthenticated<=-10:
+                    print("Verification failed!")
+                    exit()
             else:
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 name = "unknown"
@@ -69,23 +72,23 @@ def detect(target_name):
                 cv2.putText(frame, name, (x,y-10), font, 0.7, color, stroke, cv2.LINE_AA)
             #img_item = "imageko.png"
             #cv2.imwrite(img_item, roi_gray)
-            
+
             color = (255, 0,0)
             stroke = 2
             end_cord_x = x+w
             end_cord_y = y+h
             cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y),color,stroke)
-            
+
             #eyes = eye_cascade.detectMultiScale(roi_gray)
             #for (ex,ey,ew,eh) in eyes:
-                #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)        
+                #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         cv2.imshow('frame',frame)
         if cv2.waitKey(20) & 0xFF == ord('q'):
         	break
         	sys.exit()
         if cv2.waitKey(20) & 0xFF == ord('q') or isAthenticated>=10:
         	break
-    
+
 
     from collections import Counter
     def MostCommon(lst):
@@ -96,10 +99,7 @@ def detect(target_name):
     print(cur_person)
     with open('logs.json','w') as f:
     	json.dump(cur_person,f)
-    	
+
     cap.release()
     cv2.destroyAllWindows()
     dashboard.openko()
-
-
-
